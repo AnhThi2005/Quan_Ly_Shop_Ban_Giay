a88c4c8e8c1eb553df7c11451ac09114d8cdb392
Phan Thi Bich Tram <0306231166@caothang.edu.vn>
2025-10-18 23:14:35 +0700

    Revert "BT_CapNhatGiaoDien"

--- Do_An_Mon_Hoc/UC_QuanLyTaiKhoanNguoiDung.Designer.cs
+++ Do_An_Mon_Hoc/UC_QuanLyTaiKhoanNguoiDung.Designer.cs
@@ -28,79 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_taiLen = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // btn_taiLen
-            // 
-            this.btn_taiLen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_taiLen.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_taiLen.Location = new System.Drawing.Point(472, 193);
-            this.btn_taiLen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btn_taiLen.Name = "btn_taiLen";
-            this.btn_taiLen.Size = new System.Drawing.Size(87, 28);
-            this.btn_taiLen.TabIndex = 1;
-            this.btn_taiLen.Text = "Tải lên";
-            this.btn_taiLen.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(290, 118);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 28);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Tải lên";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(454, 277);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 28);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Tải lên";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(272, 202);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(87, 28);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Tải lên";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // UC_QLSanPham
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btn_taiLen);
-            this.Name = "UC_QLSanPham";
-            this.Size = new System.Drawing.Size(1031, 415);
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btn_taiLen;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
